b475a910c221641b68c5417bedd4650bc073bf8a
SUVASIS <suvasis@SUVASISs-MacBook-Pro.local>
2023-01-17 16:42:45 +0530

    code

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>
+          Download IOS Applicatoion
+        </h2>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="./application/Aerodynamic.ipa"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          >
+          Download Application
         </a>
       </header>
     </div>
