8bc74a31b71473816233ea2575fb33a222e520fb
SUVASIS <suvasis@SUVASISs-MacBook-Pro.local>
2023-01-17 18:01:38 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
         <h2>
           Download IOS Applicatoion
         </h2>
-        <a
-          className="App-link"
-          href="./application/Aerodynamic.ipa"
-          target="_blank"
-          rel='noopener noreferrer'
-          >
-          Download Application
-        </a>
+        
         <a href="itms-services://?action=download-manifest&amp; 
     url=https://aerodynamic.vercel.app/application/manifest.plist" className="App-link" 
     target="_blank"
